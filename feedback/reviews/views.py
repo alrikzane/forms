7a8b8060feb73e55062b7292d15ca1be683b3c98
alrikzane <ckill071@gmail.com>
2022-07-28 14:38:15 +0300

    add handling data in view and validation in views and html

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,4 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 def review(request):
-    return render(request, 'reviews/review.html')
+    if request.method == "POST":
+        entered_username = request.POST['name']
+        if entered_username == "":
+            return render(request, 'reviews/review.html', {
+                'error' : True
+            })
+        print(entered_username)
+        return HttpResponseRedirect('/thank-you')
+    else:
+        
+        return render(request, 'reviews/review.html', {
+                'error' : False
+            })
+
+
+def thanks(request):
+    return render(request, 'reviews/ty.html')
