90b61506d03039ec5ab893d09ef149c3e8e83c10
alrikzane <ckill071@gmail.com>
2022-07-29 17:37:18 +0300

    add form, model for form and import it into view

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,4 +1,9 @@
-from django import forms 
+from django import forms
+
+
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your name", max_length=100, min_length=2)
+    rating = forms.IntegerField(label="Your rating", max_value=5, min_value=1)
+    review_text = forms.CharField(label='Your feedback', widget=forms.Textarea, max_length=300)
+
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            data = form.cleaned_data
+            review = Review(user_name=data['user_name'], rating=data['rating'], review_text=data['review_text'])
+            review.save()
             return HttpResponseRedirect('/thank-you')
-
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
     return render(request, 'reviews/review.html', {
             'form' : form
         })
