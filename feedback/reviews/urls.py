166de863a4324bceec6abe0b8941bbf160b630ea
alrikzane <ckill071@gmail.com>
2022-08-02 07:10:38 +0300

    remove unused code

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ReviewView.as_view()),
     path('thank-you', views.ThankYou.as_view()),
-    path('reviews', views.ReviewsView.as_view())    
+    path('reviews', views.ReviewsView.as_view()),
+    path('reviews/<int:id>', views.DetailedReviewView.as_view(), name='review-detail')     
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,32 +1,19 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
-from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from .models import Review
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, 'reviews/review.html', {
-            'form' : form
-        })
-    def post(self, request):
-        form=ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
-        return render(request, 'reviews/review.html', {
-            'form' : form
-        })
 
+
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank-you'
 
 
 class ThankYou(TemplateView):
     template_name = 'reviews/ty.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['message'] = "This works"
-        return context
 
 
 class ReviewsView(TemplateView):
@@ -34,4 +21,12 @@ class ReviewsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['reviews'] = Review.objects.all()
+        return context
+
+
+class DetailedReviewView(TemplateView):
+    template_name = 'reviews/review-detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['review'] = Review.objects.get(id=kwargs['id'])
         return context
