9c08be66d167c37d3f2a9e7556fac4c9d8659ae4
alrikzane <ckill071@gmail.com>
2022-08-06 08:50:47 +0300

    add sessions for liked reviews feature

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ReviewView.as_view()),
     path('thank-you', views.ThankYou.as_view()),
     path('reviews', views.ReviewsView.as_view()),
-    path('reviews/<int:id>', views.DetailedReviewView.as_view(), name='review-detail')     
+    path('reviews/liked', views.LikeView.as_view()),
+    path('reviews/<int:pk>', views.DetailedReviewView.as_view(), name='review-detail')     
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,8 +1,12 @@
+from urllib import request
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 from .models import Review
+from django.views import View
 
 
 class ReviewView(CreateView):
@@ -24,9 +28,20 @@ class ReviewsView(TemplateView):
         return context
 
 
-class DetailedReviewView(TemplateView):
+class DetailedReviewView(DetailView):
     template_name = 'reviews/review-detail.html'
+    model = Review
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['review'] = Review.objects.get(id=kwargs['id'])
+        loaded_review = self.object
+        request = self.request
+        context["is_liked"] = request.session.get('liked_review') == loaded_review.id
         return context
+    
+
+class LikeView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['liked_review'] = int(review_id)
+        return HttpResponseRedirect('/reviews/'+review_id)
+
